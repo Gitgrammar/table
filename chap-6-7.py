81f1eb9ba7286c1152b55361459d6a24461b275a
hogehoge <elementrd6@hotmail.com>
2022-05-24 09:13:28 +0900

    jupiterで表表示

--- chap-6-7.py
+++ chap-6-7.py
@@ -1,14 +1,30 @@
 import pandas
+from matplotlib import pyplot
 df= pandas.read_csv(
 		'data.csv',
-		encoding='Shift_JIS',
+		encoding='Shift_JIS'
 )
+df_stations= pandas.read_csv(
+		'amedas_stations.csv',
+		encoding='Shift_JIS',index_col=0
+)
+df=df.join(
+		df_stations,
+		on='station'
+)
+
+print(df[
+				(df['station'].isin(['東京','札幌'])) & 
+        (df['temp']<20)
+			]
+		)
+print((df.plot.scatter('latitude','temp')))
+print((df.plot.scatter('temp','latitude')))
+
 df_sliced=df.iloc[1:4]
 series_temp =df['temp']
 
 mean_temp=series_temp.mean()
 
 series_filled=series_temp.fillna(mean_temp)
-df['temp']=series_filled
-print(df)
 		
